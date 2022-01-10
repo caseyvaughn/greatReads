eb50680df7305c181926f3bf40f0d440c2f69e5c
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-01-10 10:40:54 -0500

    added api base and key

---
+++ client/src/services/apiConfig/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const BASE = process.env.REACT_APP_AIRTABLE_BASE;
+const KEY = process.env.REACT_APP_AIRTABLE_KEY;
+
+const baseURL = `https://api.airtable.com/v0/${BASE}/Books`;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${KEY}`,
+  }
+})
+
+export default api;

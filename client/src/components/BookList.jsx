a65052ba8cbd61f6c07971cd68f3afdeea0ab5b6
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-01-10 11:02:00 -0500

    able to display book title on main page

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -10,14 +10,25 @@ export default function BookList() {
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await api.get();
-      // console.log(res.data.records);
-      console.log(res.data);
+      console.log(res.data.records);
+      setBooks(res.data.records);
     }
     fetchBooks();
   }, [])
   return (
     <div>
-      Books
+      <ul>
+        {books.map((book) => {
+          return (
+            <li key={book.id}>
+              <div>
+                <h3>{book.fields.title}</h3>
+                <h4>{book.fields.author}</h4>
+              </div>
+            </li>
+        )
+      })}
+      </ul>
     </div>
   )
 }

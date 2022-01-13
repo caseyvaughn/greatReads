5fe6827bb52d784ff4bb968b12397ac0fd7a6c89
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-01-13 16:35:05 -0500

    formatted book cover into bookstrap card

--- client/src/components/GoogleBooksSearch.jsx
+++ client/src/components/GoogleBooksSearch.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react'
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
+import { Row, Col, Container } from "react-bootstrap";
 import api from '../services/apiConfig/index.js';
 
 export default function GoogleBooksSearch() {
@@ -18,14 +19,10 @@ export default function GoogleBooksSearch() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(book);
-
     let googleBooksURL = `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${GOOGLE_API_KEY}&maxResults=5`
-    
     //try to add intitle: as a search term!
     axios.get(googleBooksURL)
       .then(data => {
-        // console.log(data)
         console.log(data.data.items);
         setResult(data.data.items);
     } )
@@ -39,9 +36,13 @@ export default function GoogleBooksSearch() {
       {result.map((book) => {
         return (
           <div>
-            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
-            <h3>{book.volumeInfo.authors}</h3>
-            <h3>{book.volumeInfo.title}</h3>
+            <Card style={{width: "200px" }}>
+              <Card.Img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} variant="top" className="book-cover" />
+                <Card.Body>
+                  <Card.Text>{book.volumeInfo.title}</Card.Text>
+                  <Card.Text>{book.volumeInfo.authors}</Card.Text>
+                </Card.Body>
+              </Card>
           </div>
         )
       }
